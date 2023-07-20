33ed2d503d141478bc4038772dafd1c6183ca7d0
micheal00000 <michealhany991@gmail.com>
2023-07-20 19:28:30 +0300

    create a style class

--- lib/features/home/persention/views/widgets/Custom-list-view-item.dart
+++ lib/features/home/persention/views/widgets/Custom-list-view-item.dart
@@ -7,33 +7,13 @@ class CustomListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7 / 4,
+      aspectRatio: 4 / 6,
       child: Container(
         decoration: BoxDecoration(
-            // color: Colors.red,
+            color: Colors.red,
             borderRadius: BorderRadius.circular(16),
             image: const DecorationImage(
                 fit: BoxFit.fill, image: AssetImage(AssestsData.testImage))),
-      ),
-    );
-  }
-}
-
-class FeatureBooksListView extends StatelessWidget {
-  const FeatureBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItem());
-        },
       ),
     );
   }
--- lib/features/home/persention/views/widgets/home_view_bdy.dart
+++ lib/features/home/persention/views/widgets/home_view_bdy.dart
@@ -1,5 +1,7 @@
+import 'package:book_store/core/utils/style.dart';
 import 'package:book_store/features/home/persention/views/widgets/Custom-list-view-item.dart';
 import 'package:book_store/features/home/persention/views/widgets/Custom_app_bar.dart';
+import 'package:book_store/features/home/persention/views/widgets/Featured-list-view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,8 +9,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeatureBooksListView()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureBooksListView(),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Best Seller',
+            style: Style.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
