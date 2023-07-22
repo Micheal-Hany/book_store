f930f10fc936c4288bedafb8fa7a6883437ff673
micheal00000 <michealhany991@gmail.com>
2023-07-22 11:42:32 +0300

    best seller list view done

--- lib/features/home/persention/views/widgets/home_view_bdy.dart
+++ lib/features/home/persention/views/widgets/home_view_bdy.dart
@@ -4,6 +4,7 @@ import 'package:book_store/core/utils/style.dart';
 import 'package:book_store/features/home/persention/views/widgets/Custom-list-view-item.dart';
 import 'package:book_store/features/home/persention/views/widgets/Custom_app_bar.dart';
 import 'package:book_store/features/home/persention/views/widgets/Featured-list-view.dart';
+import 'package:book_store/features/home/persention/views/widgets/best%20seller%20list%20view.dart';
 import 'package:book_store/features/home/persention/views/widgets/best-seller-List-view-item.dart';
 import 'package:flutter/material.dart';
 
@@ -12,26 +13,31 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeatureBooksListView(),
-          SizedBox(
-            height: 40,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              FeatureBooksListView(),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Best Seller',
+                style: Style.textStyle18,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: Style.textStyle18,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          BestSellerListViewItem()
-        ],
-      ),
+        ),
+        SliverToBoxAdapter(
+          child: BestSellerListView(),
+        )
+      ],
     );
   }
 }
