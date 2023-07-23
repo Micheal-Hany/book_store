33118b72e2d4dff1a59677eb76eea1de9fcaf08a
micheal00000 <michealhany991@gmail.com>
2023-07-23 12:53:41 +0300

    create return type of home repo method

--- lib/features/home/data/repos/home-repo.dart
+++ lib/features/home/data/repos/home-repo.dart
@@ -1,4 +1,8 @@
+import 'package:book_store/core/errors/Failure.dart';
+import 'package:book_store/features/home/data/Models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetkchBestSellerBooks();
-  fetkchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetkchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetkchFeaturedBooks();
 }
