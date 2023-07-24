4a6fcb2df91b50967d23d4943ef970cf837f2fa0
micheal00000 <michealhany991@gmail.com>
2023-07-24 11:40:10 +0300

    create Featured book method

--- lib/features/home/persention/Manger/Featured Book cubit/featured_book_cubit.dart
+++ lib/features/home/persention/Manger/Featured Book cubit/featured_book_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:book_store/core/errors/Failure.dart';
 import 'package:book_store/features/home/data/Models/book_model/book_model.dart';
+import 'package:book_store/features/home/data/repos/home-repo.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_book_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.homeRepo) : super(FeaturedBookInitial());
+  final HomeRepo homeRepo;
+  Future<void> fatchDataFeaturedBooks() async {
+    emit(FeaturedBookLoading());
+
+    var result = (await homeRepo.fetkchFeaturedBooks());
+    result.fold((failure) {
+      emit(FeaturedBookFailure(failure.errorMasssage));
+    }, (booksList) => emit(FeaturedBookSuccess(booksList)));
+  }
 }
