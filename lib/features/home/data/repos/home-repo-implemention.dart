96db03d4866799415987151a5b387353f53b74d5
micheal00000 <michealhany991@gmail.com>
2023-07-24 10:08:26 +0300

    implement newest books function

--- lib/features/home/data/repos/home-repo-implemention.dart
+++ lib/features/home/data/repos/home-repo-implemention.dart
@@ -1,13 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:book_store/core/utils/api-service.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
 import 'package:book_store/core/errors/Failure.dart';
 import 'package:book_store/features/home/data/Models/book_model/book_model.dart';
 import 'package:book_store/features/home/data/repos/home-repo.dart';
-import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
+
 class HomeRepoImpl implements HomeRepo {
+  ApiService apiService;
+  HomeRepoImpl({
+    required this.apiService,
+  });
   @override
-  Future<Either<Failure, List<BookModel>>> fetkchBestSellerBooks() {
-    Dio().get('');
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetkchNewstBooks() async {
+    try {
+      var data =
+          await apiService.get(endPoint: 'volumes?q=subject:computer science');
+      List<BookModel> bookList = [];
+      // for (int i = 0; i < data['items'].length; i++) {
+      //   bookList.add(data['items'][i])
+      // }
+      for (var item in data['items']) {
+        bookList.add(BookModel.fromJson(item));
+      }
+      return right(bookList);
+    } on Exception {
+      return left(ServerFalure());
+    }
   }
 
   @override
--- lib/features/home/data/repos/home-repo.dart
+++ lib/features/home/data/repos/home-repo.dart
@@ -3,6 +3,6 @@ import 'package:book_store/features/home/data/Models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetkchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetkchNewstBooks();
   Future<Either<Failure, List<BookModel>>> fetkchFeaturedBooks();
 }
